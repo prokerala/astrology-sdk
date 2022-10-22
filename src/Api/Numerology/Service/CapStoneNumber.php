eb0df1602aa59d9458ed04de40db7a47a523c4b6
Vishnu K P <vishnu.p@ennexa.com>
2022-10-22 17:56:12 +0530

    feat: adding service and result for cap stone number

---
+++ src/Api/Numerology/Service/CapStoneNumber.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of Prokerala Astrology API PHP SDK
+ *
+ * © Ennexa Technologies <info@ennexa.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Prokerala\Api\Numerology\Service;
+
+use Prokerala\Api\Astrology\Traits\Service\TimeZoneAwareTrait;
+use Prokerala\Api\Astrology\Transformer;
+use Prokerala\Api\Numerology\Result\CapStoneResult;
+use Prokerala\Api\Numerology\Result\CornerStoneResult;
+use Prokerala\Api\Numerology\Result\DestinyResult;
+use Prokerala\Api\Numerology\Result\InclusionTableResult;
+use Prokerala\Common\Api\Client;
+use Prokerala\Common\Api\Exception\QuotaExceededException;
+use Prokerala\Common\Api\Exception\RateLimitExceededException;
+use Prokerala\Common\Traits\Api\ClientAwareTrait;
+
+final class CapStoneNumber
+{
+    use ClientAwareTrait;
+    use TimeZoneAwareTrait;
+
+    /** @var string */
+    protected $slug = '/numerology/cap-stone-number';
+
+    /** @var Transformer<CapStoneResult> */
+    private $transformer;
+
+    /**
+     * @param Client $client Api client
+     */
+    public function __construct(Client $client)
+    {
+        $this->apiClient = $client;
+        $this->transformer = new Transformer(CapStoneResult::class);
+        $this->addDateTimeTransformer($this->transformer);
+    }
+
+    /**
+     * Fetch result from API.
+     *
+     * @throws QuotaExceededException
+     * @throws RateLimitExceededException
+     **
+     * @return CapStoneResult
+     */
+    public function process(string $firstName, string $middleName, string $lastName)
+    {
+        $parameters = [
+            'first_name' => $firstName,
+            'middle_name' => $middleName,
+            'last_name' => $lastName,
+        ];
+
+        $apiResponse = $this->apiClient->process($this->slug, $parameters);
+
+        return $this->transformer->transform($apiResponse->data);
+    }
+}
