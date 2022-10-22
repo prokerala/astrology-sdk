fcd99c48b9f50fcd10efaa72e525685c4900f775
Vishnu K P <vishnu.p@ennexa.com>
2022-10-22 17:55:33 +0530

    feat: adding service and result for corner stone number

---
+++ src/Api/Numerology/Result/CornerStoneResult.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types=1);
+
+namespace Prokerala\Api\Numerology\Result;
+use JsonSerializable;
+
+class CornerStoneResult implements JsonSerializable
+{
+    /**
+     * @var int $id
+     */
+    private $id;
+    /**
+     * @var string $title
+     */
+    private $title;
+    /**
+     * @var Number $number
+     */
+    private $number;
+    /**
+     * @var NameResult $nameResult
+     */
+    private $nameResult;
+    /**
+     * @param int $id
+     * @param string $title
+     * @param Number $number
+     * @param NameResult $nameResult
+     */
+    public function __construct($id, $title, $number, $nameResult) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->number = $number;
+        $this->nameResult = $nameResult;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Number
+     */
+    public function getNumber(): Number
+    {
+        return $this->number;
+    }
+
+    /**
+     * @return NameResult
+     */
+    public function getNameResult(): NameResult
+    {
+        return $this->nameResult;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' =>$this->id,
+            'title' =>$this->title,
+            'number' => $this->number,
+            'name_result' => $this->nameResult,
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+}
