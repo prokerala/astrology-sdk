6445e773bf9054d1ae33f427d9a392f5ca6f1b86
Akhil P <akhil.p@ennexa.com>
2022-11-08 18:05:24 +0530

    fix: created sdk for yoga details

---
+++ src/Api/Astrology/Result/Horoscope/Yoga.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of Prokerala Astrology API PHP SDK
+ *
+ * © Ennexa Technologies <info@ennexa.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Prokerala\Api\Astrology\Result\Horoscope;
+
+use Prokerala\Api\Astrology\Result\ResultInterface;
+use Prokerala\Api\Astrology\Traits\Result\RawResponseTrait;
+
+final class Yoga implements ResultInterface
+{
+    use RawResponseTrait;
+
+    /**
+     * @var Yoga\AdvancedYogaDetails[]
+     */
+    private $yogas;
+
+    /**
+     * Kundli constructor.
+     *
+     * @param Yoga\AdvancedYogaDetails[] $yogas
+     */
+    public function __construct(array $yogas)
+    {
+        $this->yogas = $yogas;
+    }
+
+    /**
+     * @return Yoga\AdvancedYogaDetails[]
+     */
+    public function getYogas()
+    {
+        return $this->yogas;
+    }
+}
