4114a5934690a9785b5bcc3a8d9418fed9c3d8ad
Georgekutty Jose <georgekutty.jose@ennexa.com>
2025-07-07 15:24:50 +0530

    chore: added upagraha position to sdk

---
+++ src/Api/Astrology/Service/UpagrahaPosition.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of Prokerala Astrology API PHP SDK
+ *
+ * © Ennexa Technologies <info@ennexa.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Prokerala\Api\Astrology\Service;
+
+use Prokerala\Api\Astrology\Location;
+use Prokerala\Api\Astrology\Result\Horoscope\UpagrahaPosition as UpagrahaPositionResult;
+use Prokerala\Api\Astrology\Traits\Service\AyanamsaAwareTrait;
+use Prokerala\Api\Astrology\Traits\Service\TimeZoneAwareTrait;
+use Prokerala\Api\Astrology\Transformer;
+use Prokerala\Common\Api\Client;
+use Prokerala\Common\Api\Exception\QuotaExceededException;
+use Prokerala\Common\Api\Exception\RateLimitExceededException;
+use Prokerala\Common\Api\Traits\ClientAwareTrait;
+
+final class UpagrahaPosition
+{
+    use AyanamsaAwareTrait;
+    use ClientAwareTrait;
+
+    /** @use TimeZoneAwareTrait<UpagrahaPositionResult> */
+    use TimeZoneAwareTrait;
+
+    protected string $slug = '/astrology/upagraha-position';
+
+    /** @var Transformer<UpagrahaPositionResult> */
+    private \Prokerala\Api\Astrology\Transformer $transformer;
+
+    /**
+     * @param Client $client Api client
+     */
+    public function __construct(Client $client)
+    {
+        $this->apiClient = $client;
+        $this->transformer = new Transformer(UpagrahaPositionResult::class);
+        $this->addDateTimeTransformer($this->transformer);
+    }
+
+    /**
+     * Fetch result from API.
+     *
+     * @param Location           $location Location details
+     * @param \DateTimeInterface $datetime Date and time
+     *
+     * @throws QuotaExceededException
+     * @throws RateLimitExceededException
+     **
+     */
+    public function process(
+        Location           $location,
+        \DateTimeInterface $datetime,
+        string             $upagrahaPosition = 'middle',
+        string             $la = 'en'
+    ): UpagrahaPositionResult {
+        $parameters = [
+            'datetime' => $datetime->format('c'),
+            'coordinates' => $location->getCoordinates(),
+            'ayanamsa' => $this->getAyanamsa(),
+            'upagraha_position' => $upagrahaPosition,
+            'la' => $la,
+        ];
+
+        $apiResponse = $this->apiClient->process($this->slug, $parameters);
+        assert($apiResponse instanceof \stdClass);
+
+        return $this->transformer->transform($apiResponse->data);
+    }
+}
